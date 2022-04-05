aa9ae49fffbdd323c04786c3952fa20671016fcc
Bruno Maroneze <maronezebruno@yahoo.com.br>
2022-04-04 22:00:37 -0400

    First version April 04, 2022

---
+++ app.R
@@ -0,0 +1,189 @@
+library(shiny)
+library(shinythemes)
+library(tidyverse)
+library(DT)
+#library(data.table)
+#library(collapsibleTree)
+library(markdown)
+
+# Lê a base de dados do dicionário e corrige o erro da codificação UTF-8
+data <- read.csv2("./data/DadosDoDicionario.csv", encoding = "UTF-8")
+colnames(data) <- c("ID","Headword", "FirstAttestationDate", "FirstAttestationExampleMD", "VariantSpellings", "Etymology", "WClass")
+
+# Lê o arquivo com as definições
+definitions <- read.csv2("./data/definitions.csv", encoding = "UTF-8")
+
+# Lê o córpus etiquetado
+TokTextDF <- read.csv2("./data/DataframePrincipal.csv", encoding = "UTF-8")
+
+# Lê os metadados do córpus
+CorpusMetadata <- read.csv2("./data/CorpusTextsMetadata.csv", encoding = "UTF-8")
+colnames(CorpusMetadata) <- c("Filename", "Author", "Title", "DateOfPublication")
+
+# Cria uma função que retorna os contextos de uma palavra consultada, com a palavra negritada,
+#e formata para mostrar; os parâmetros são a palavra-entrada e o número da acepção
+ConsultaAosContextos <- function(InputConsulta, SenseNumber){
+  ContextosTexto <- as.list(TokTextDF$sentence[TokTextDF$lemma == InputConsulta & TokTextDF$sensenumber==SenseNumber])
+  # Retorna uma lista de contextos
+
+  ContextosTextoNegritados <- NULL
+  for(a in 1:length(ContextosTexto)){ # inclui negrito nas palavras buscadas
+    ContextosTextoNegritados[a] <- sub(TokTextDF$token[TokTextDF$lemma == InputConsulta & TokTextDF$sensenumber==SenseNumber][a],
+                                       paste0("<b>",
+                                       TokTextDF$token[TokTextDF$lemma == InputConsulta & TokTextDF$sensenumber==SenseNumber][a], 
+                                       "</b>"), ContextosTexto[a])
+    Autor <- CorpusMetadata$Author[CorpusMetadata$Filename == TokTextDF$doc_id[TokTextDF$lemma == InputConsulta][a]]
+    Ano <- CorpusMetadata$DateOfPublication[CorpusMetadata$Filename == TokTextDF$doc_id[TokTextDF$lemma == InputConsulta][a]]
+    ContextosTextoNegritados[a] <- paste0(a, " - ", ContextosTextoNegritados[a],
+                                          " (", Autor, ", ", Ano,")<br>")
+    
+  }
+
+  return(ContextosTextoNegritados)
+}
+
+
+ui <- fluidPage(
+  navbarPage(id="Dict", title=HTML("Dicionário Histórico de Termos da Biologia"), # Dá para fazer
+                                                                                  # o título linkar 
+                                                            # para a página inicial assim:
+                                                        # title=tags$a(href="https://brunomaroneze.shinyapps.io/Dicio_Botanica/"
+             
+             windowTitle="Dicionário Histórico de Termos da Biologia", 
+             collapsible = TRUE, inverse = FALSE, theme = shinytheme("readable"),
+             tabPanel(id="Home","Home", fluid = TRUE,       
+                      fluidRow(
+                        column(width=4,offset=1, htmlOutput("ProjectIntro")),
+                        column(width=6,offset=1, img(src = "VandelliTabXV.png", height = "500px"),
+                               HTML("Tabela XV - Vandelli - <i>Diccionario de Termos Technicos de Historia Natural</i> (1788)")
+                               ,tags$hr(),
+                               HTML("<a href=\"https://creativecommons.org/licenses/by-nc-sa/4.0/\"><img src=\"creativecommons.png\" height=\"31px\"></a>")
+                               ,
+                               HTML("<p style=\"font-size:10px\">O <b>Dicionário Histórico de Termos da Biologia</b>
+                               está licenciado sob a <a href=\"https://creativecommons.org/licenses/by-nc-sa/4.0/\">Licença Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+                               International</a></p>"),
+                               tags$br(),
+                               HTML("<p style=\"font-size:10px\">The <b>Historical Dictionary of Biology Terms</b>
+                               is licenced under a <a href=\"https://creativecommons.org/licenses/by-nc-sa/4.0/\">Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+                               International Licence</a></p>")
+                               )  
+                      )
+             ),
+             
+             tabPanel(id="Portuguese Search","Consulta", fluid = TRUE,  
+                      
+                      sidebarLayout(sidebarPanel(width = 3,
+                                                 selectInput(inputId = "headword",
+                                                             list(label = "Selecione uma entrada "),
+                                                             choices = c(sort(as.character(data$Headword))), 
+                                                             selected = "antera",
+                                                             multiple = FALSE,
+                                                             size = 10,
+                                                             selectize = FALSE)
+                                                 ),
+                                    mainPanel(width = 9,
+                                                        htmlOutput("Entry"),
+                                                        tags$hr(),
+                                                        htmlOutput("Definition"),
+                                                        tags$hr(),
+                                                        htmlOutput("Etymo"),
+                                                        tags$hr(),
+                                                        htmlOutput("FirstAttestation"),
+                                                        tags$br()
+                                                        
+                                              )    
+                                                  )
+                                                  
+                                      )
+                                    )
+                      
+                                    )
+                      
+
+server <- function(input, output, session) {
+  
+# Descobri que não precisa desse updateSelectizeInput porque o input não vai ficar mudando
+#  updateSelectizeInput(session, "headword", choices=c(sort(as.character(data$Headword))),
+#                       selected="antera")
+  
+  
+  EntryData <- reactive({
+    
+    EntryData <- as.list(data[data$Headword==input$headword,])
+    EntryData
+  })
+  
+  
+  output$ProjectIntro <-renderText({
+    includeMarkdown("ProjectIntro.Rmd")
+  })
+  
+  output$Entry <- renderText({
+    
+
+    paste0("<font size='+2'><b>", input$headword, "</font></b><br>"
+           , data$WClass[data$Headword==input$headword]
+           )
+    
+    
+  })
+    
+  output$Definition <- renderText({
+
+    Definition <- NULL
+    for(c in 1:sum(definitions$Headword == input$headword)){
+      
+      Definicao <- definitions$Definition[definitions$Headword == input$headword][c]
+      Definition[c] <- paste0(c, ". ", Definicao, "<br><details><summary>Exemplos (<u>clique para expandir</u>)</summary><font size= '-1'>",
+             paste(ConsultaAosContextos(input$headword, c), collapse = ""), "</font></details><br>")
+    }
+    
+    Definition
+    
+  })
+  
+  
+  output$Etymo <- renderText({
+    
+    Etymology <- EntryData()$Etymology
+    paste("<b>Discussão histórico-etimológica</b><br>",
+          Etymology)
+  }) 
+  
+  
+  output$FirstAttestation <- renderText({
+    
+    FirstAttestationDate <- EntryData()$FirstAttestationDate
+    FirstAttestationExampleMD <- EntryData()$FirstAttestationExampleMD
+    VariantSpellings <- EntryData()$VariantSpellings
+    if (!is.na(VariantSpellings)){
+    paste0("<font color='steelblue'>",input$headword ,"</font>, também grafado <font color='steelblue'>", VariantSpellings ,"</font>, é atestado em <b>",FirstAttestationDate,"</b>: ", FirstAttestationExampleMD)
+    }else{
+      paste0("<font color='steelblue'>",input$headword ,"</font> é atestado em <b>",FirstAttestationDate,"</b>: ", FirstAttestationExampleMD)
+      
+    }
+  }) 
+
+#  output$NumDeContextos <- renderText({
+
+    #  Criar trecho para incluir variantes gráficas
+    
+    # if(existe variante gráfica){
+    
+    #     paste0("O termo <b>", input$headword, "</b> também é grafado ", "Y...", "(ver contextos abaixo)")
+    
+    # } else {
+    
+    # entra o outro "if" dos contextos que já existe aí embaixo
+    
+    # }
+    
+    
+
+#  })
+  
+
+  
+}
+
+shinyApp(ui = ui, server = server)
