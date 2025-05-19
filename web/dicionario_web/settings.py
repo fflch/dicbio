0dcb4269a942339cdbb57277df2cf6884c7857da
brunomaroneze <bruno.maroneze@gmail.com>
2025-05-18 20:18:06 -0400

    Commit inicial do Django

--- web/dicionario_web/settings.py
+++ web/dicionario_web/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pagina_inicial',
+    'verbetes',
+    'documentacao',
+    'diversos',
+    'corpus_digital',
 ]
 
 MIDDLEWARE = [
