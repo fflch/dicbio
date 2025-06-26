8b1c11bbfb07f8a41cd1f9a1b95339327a16d2ae
Ricardo Fontoura <ricardfo@usp.br>
2025-06-26 12:29:53 -0300

    add re_path para mostrar imagens.

--- dicbio/urls.py
+++ dicbio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -23,4 +25,10 @@ urlpatterns = [
     path('documentacao/', include('documentacao.urls', namespace='documentacao')),
     path('consulta/', include('verbetes.urls', namespace='verbetes')),
     path('corpus/', include('corpus_digital.urls', namespace='corpus_digital')),
+]
+
+urlpatterns += [
+    re_path(r'static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT
+    })
 ]
